00a034701bfca4a91d0f7996f0573959636d6b39
Danil Uktus <damayorov93@gmail.com>
2025-03-31 00:59:58 +0500

    first point in progress

--- NailBot/Init.cs
+++ NailBot/Init.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using static NailBot.Extensions;
 using static NailBot.StartValues;
+using Otus.ToDoList.ConsoleBot;
+using Otus.ToDoList.ConsoleBot.Types;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace NailBot
 {
@@ -32,15 +35,15 @@ namespace NailBot
         static string userName = "Пользователь";
 
         //объявление списка задач в виде List
-        static List<string> tasksList = new List<string>();
-        
+        public static List<string> tasksList = new List<string>();
+
         //объявление списка задач в виде Array
-        static string[] tasksArray = [];
+        public static string[] tasksArray = [];
 
 
 
-        //МЕТОДЫ КОМАНД
-        //метод команды Help
+        ////МЕТОДЫ КОМАНД
+        ////метод команды Help
         internal static void ShowHelp(string userName, bool availableEcho)
         {
             if (availableEcho)
@@ -68,26 +71,26 @@ namespace NailBot
             }
         }
 
-        //метод команды Info
+        ////метод команды Info
         internal static void ShowInfo()
         {
             DateTime releaseDate = new DateTime(2025, 02, 08);
             Console.WriteLine("Это NailBot версии 1.0 Beta. Релиз {0:dd MMMM yyyy}", releaseDate);
         }
 
-        //работа с List
-        //метод добавления задачи в List
-        internal static void AddTaskList(List<string> tasks)
-        {
-            Console.WriteLine("Введите описание задачи (добавится в List):");
-            string newTask = Validate.ValidateString(Console.ReadLine());
+        ////работа с List
+        ////метод добавления задачи в List
+        //internal static void AddTaskList(List<string> tasks)
+        //{
+        //    Console.WriteLine("Введите описание задачи (добавится в List):");
+        //    string newTask = Validate.ValidateString(Console.ReadLine());
 
-            tasks.Add(newTask);
+        //    tasks.Add(newTask);
 
-            Console.WriteLine($"Задача \"{newTask}\" добавлена в List задач");
-        }
+        //    Console.WriteLine($"Задача \"{newTask}\" добавлена в List задач");
+        //}
 
-        //добавлю перегрузку метода добавления задачи в List
+        ////добавлю перегрузку метода добавления задачи в List
         internal static void AddTaskList(List<string> tasks, int maxTasksAmount, int taskLengthLimit)
         {
             //проверяю длину листа и выбрасываю исключение если больше лимита
@@ -109,7 +112,7 @@ namespace NailBot
             Console.WriteLine($"Задача \"{newTask}\" добавлена в List задач");
         }
 
-        //метод рендера задач из List
+        ////метод рендера задач из List
         internal static void ShowTasks(List<string> tasks)
         {
             if (tasks.Count == 0)
@@ -126,7 +129,7 @@ namespace NailBot
             }
         }
 
-        //метод удаления задачи из List
+        ////метод удаления задачи из List
         internal static void RemoveTaskList(List<string> tasks)
         {
             if (tasks.Count == 0)
@@ -155,29 +158,29 @@ namespace NailBot
         }
 
 
-        //работа с Array
-        //метод добавления задачи в Array
-        internal static void AddTaskArray(ref string[] tasks)
-        {
-            string[] arrayTasks = new string[tasks.Length + 1];
+        ////работа с Array
+        ////метод добавления задачи в Array
+        //internal static void AddTaskArray(ref string[] tasks)
+        //{
+        //    string[] arrayTasks = new string[tasks.Length + 1];
 
-            Console.WriteLine("Введите описание задачи (добавится в Array):");
-            //валидация строки
-            string newTask = Validate.ValidateString(Console.ReadLine());
+        //    Console.WriteLine("Введите описание задачи (добавится в Array):");
+        //    //валидация строки
+        //    string newTask = Validate.ValidateString(Console.ReadLine());
 
-            int index = arrayTasks.Length - 1;
+        //    int index = arrayTasks.Length - 1;
 
-            arrayTasks[index] = newTask;
+        //    arrayTasks[index] = newTask;
 
-            for (int i = 0; i < index; i++)
-                arrayTasks[i] = tasks[i];
+        //    for (int i = 0; i < index; i++)
+        //        arrayTasks[i] = tasks[i];
 
-            tasks = arrayTasks;
+        //    tasks = arrayTasks;
 
-            Console.WriteLine($"Задача \"{newTask}\" добавлена в Array задач");
-        }
+        //    Console.WriteLine($"Задача \"{newTask}\" добавлена в Array задач");
+        //}
 
-        //добавлю перегрузку метода добавления задачи в Array
+        ////добавлю перегрузку метода добавления задачи в Array
         internal static void AddTaskArray(ref string[] tasks, int maxTasksAmount, int taskLengthLimit)
         {
             string[] arrayTasks = new string[tasks.Length + 1];
@@ -213,7 +216,7 @@ namespace NailBot
             Console.WriteLine($"Задача \"{newTask}\" добавлена в Array задач");
         }
 
-        //метод рендера задач из Array
+        ////метод рендера задач из Array
         internal static void ShowTasks(ref string[] tasks)
         {
             if (tasks.Length == 0)
@@ -226,7 +229,7 @@ namespace NailBot
             }
         }
 
-        //метод удаления задачи из Array
+        ////метод удаления задачи из Array
         internal static void RemoveTaskArray(ref string[] tasks)
         {
             if (tasks.Length == 0)
@@ -267,15 +270,31 @@ namespace NailBot
             }
         }
 
+        static long userId = 123;
+        static long chatId = 123321;
+        static int messageId = 0;
+
+        //static User newUser = new User { Id = ++userId, Username = "igor" };
+
+        //static Chat newChat = new Chat { Id = ++chatId };
+
+        static ConsoleBotClient bot = new ConsoleBotClient();
+
+        static ITelegramBotClient telegramBotClient = bot;
+
+
+
         public static void Start(int tasksAmount, int taskLenght)
         {
+            
             //присваиваю значения длин
             if (tasksAmount == 0)
                 maxTaskAmount = tasksAmount.GetStartValues("Введите максимально допустимое количество задач");
 
             if (maxTaskLenght == 0)
                 maxTaskLenght = taskLenght.GetStartValues("Введите максимально допустимую длину задачи");
-            
+
+
             //создаю экземпляр класса со значениями длин для хранения
             StartValues StartValues = new StartValues(maxTaskAmount, maxTaskLenght);
 
@@ -299,6 +318,10 @@ namespace NailBot
             string echoText = "";
             bool answer = true;
 
+            //регулярка на реплейс циферного значения Enum
+            //input = input.NumberReplacer();
+
+
             if (input.StartsWith("/echo "))
             {
                 echoText = input.Substring(6);
@@ -307,77 +330,110 @@ namespace NailBot
 
             //реплейс слэша для приведения к Enum
             input = input.Replace("/", string.Empty);
+            
+            //создаю экземпляр объекта сообщения
+            Message newMessage = new Message { Id = messageId++, Text = input};
 
-            Commands command;
 
-            //регулярка на реплейс циферного значения Enum
-            input = input.NumberReplacer();
+            //создаю экземпляр объекта хендлера
+            UpdateHandler handler = new UpdateHandler();
 
-            //приведение к типу Enum
-            if (Enum.TryParse<Commands>(input, true, out var result))
-                command = result;
-            else
-                command = default;
+            //объявляю переменную типа интерфейса
 
-            switch (command)
-            {
-                case Commands.Start:
-                    Console.WriteLine("Введите ваше имя:");
-                    userName = Console.ReadLine();
-                    if (!string.IsNullOrWhiteSpace(userName))
-                    {
-                        userName = char.ToUpper(userName[0]) + userName.Substring(1);
-                        Console.WriteLine($"Привет {userName}! Тебе стала доступна команда /echo");
-                        Console.WriteLine($"Чтобы использовать команду /echo, напиши \"/echo *свой текст*\"\n");
-                        availableEcho = true;
-                        Commands.Start.CommandsRender(availableEcho, echoNumber);
-                    }
-                    else
-                        Console.WriteLine("Привет, Незнакомец!");
-                    break;
 
-                case Commands.Help:
-                    ShowHelp(userName, availableEcho);
-                    break;
 
-                case Commands.Info:
-                    ShowInfo();
-                    break;
 
-                case Commands.Echo:
-                    Console.WriteLine(echoText);
-                    break;
+            //создаю экземпляр объекта апдейт
+            Update update = new Update { Message = newMessage };
 
-                case Commands.Addtask:
-                    //вызов метода добавления задачи в List
-                    AddTaskList(tasksList, maxTaskAmount, maxTaskLenght);
-                    //вызов метода добавления задачи в Array
-                    AddTaskArray(ref tasksArray, maxTaskAmount, maxTaskLenght);
-                    break;
+            bot.StartReceiving(handler);
 
-                case Commands.Showtasks:
-                    //вызов метода рендера задач из List
-                    ShowTasks(tasksList);
-                    //вызов метода рендера задач из Array
-                    ShowTasks(ref tasksArray);
-                    break;
+            handler.HandleUpdateAsync(telegramBotClient, update);
 
-                case Commands.Removetask:
-                    //вызов метода удаления задачи из List
-                    RemoveTaskList(tasksList);
-                    //вызов метода удаления задачи из Array
-                    RemoveTaskArray(ref tasksArray);
-                    break;
+            Console.WriteLine(input);
 
-                case Commands.Exit:
-                    answer = false;
-                    break;
-                default:
-                    Console.WriteLine("Ошибка: введена некорректная команда. Пожалуйста, введите команду заново.\n");
-                    Commands.Start.CommandsRender(availableEcho, echoNumber);
-                    break;
-            }
-            return answer;
+            return handler.answer;
         }
     }
 }
+
+//Расширение функционала приложения, разработанного в предыдущем домашнем задании:
+
+//Добавление интерфейсов и классов аналогичных Telegram API, чтобы в будущем было легче переключиться на реального Telegram бота
+//Работа с классами и интерфейсами
+//Добавление новых команд
+
+//Описание/Пошаговая инструкция выполнения домашнего задания:
+//Ссылка на описание в GitHub
+
+//Перед выполнением нужно ознакомится с Правила отправки домашнего задания на проверку
+
+//Подключение библиотеки Otus.ToDoList.ConsoleBot
+//Добавить к себе в решение и в зависимости к своему проекту с ботом проект Otus.ToDoList.ConsoleBot GitHub
+//Ознакомиться с классами в папке Types и с README.md
+//Создать класс UpdateHandler, который реализует интерфейс IUpdateHandler, и перенести в метод HandleUpdateAsync обработку всех команд. Вместо Console.WriteLine использовать SendMessage у ITelegramBotClient
+//Перенести try/catch в HandleUpdateAsync. В Main оставить catch(Exception)
+//Для вывода в коноль сообщений использовать метод ITelegramBotClient.SendMessage
+//Удалить команду /echo
+//Изменение логики команды /start
+//Не нужно запрашивать имя
+//Добавить класс User
+//Свойства
+//Guid UserId
+//long TelegramUserId
+//string TelegramUserName
+//DateTime RegisteredAt
+//Добавление класса сервиса UserService
+//Добавить интерфейс IUserService
+//interface IUserService
+//{
+//    User RegisterUser(long telegramUserId, string telegramUserName);
+//    User? GetUser(long telegramUserId);
+//}
+//Создать класс UserService, который реализует интерфейс IUserService. Заполнять telegramUserId и telegramUserName нужно из значений Update.Message.From
+//Добавить использование IUserService в UpdateHandler. Получать IUserService нужно через конструктор
+//При команде /start нужно вызвать метод IUserService.RegisterUser.
+//Если пользователь не зарегистрирован, то ему доступны только команды /help /info
+//Добавление класса ToDoItem
+//Добавить enum ToDoItemState с двумя значениями
+//Active
+//Completed
+//Добавить класс ToDoItem
+//Свойства
+//Guid Id
+//User User
+//string Name
+//DateTime CreatedAt
+//ToDoItemState State
+//DateTime? StateChangedAt - обновляется при изменении State
+//Добавить использование класса ToDoItem вместо хранения только имени задачи
+//Изменение логики /showtasks
+//Выводить только задачи с ToDoItemState.Active
+//Добавить вывод CreatedAt и Id. Пример: Имя задачи - 01.01.2025 00:00:00 - 17056344 - 0e03 - 4a21 - b0dd - f0d30a5abf49
+//Добавление класса сервиса ToDoService
+//Добавить интерфейс IToDoService
+//public interface IToDoService
+//{
+//    //Возвращает ToDoItem для UserId со статусом Active
+//    IReadOnlyList<ToDoItem> GetActiveByUserId(Guid userId);
+//    ToDoItem Add(User user, string name);
+//    void MarkCompleted(Guid id);
+//    void Delete(Guid id);
+//}
+//Создать класс ToDoService, который реализует интерфейс IToDoService. Перенести в него логику обработки команд
+//Добавить использование IToDoService в UpdateHandler. Получать IToDoService нужно через конструктор
+//Изменить формат обработки команды /addtask. Нужно сразу передавать имя задачи в команде. Пример: / addtask Новая задача
+//Изменить формат обработки команды /removetask. Нужно сразу передавать номер задачи в команде. Пример: / removetask 2
+//Добавление команды /completetask
+//Добавить обработку новой команды /completetask. При обработке команды использовать метод IToDoService.MarkAsCompleted
+//Пример: / completetask 73c7940a - ca8c - 4327 - 8a15 - 9119bffd1d5e
+//Добавление команды /showalltasks
+//Добавить обработку новой команды /showalltasks. По ней выводить команды с любым State и добавить State в вывод
+//Пример: (Active)Имя задачи - 01.01.2025 00:00:00 - ffbfe448 - 4b39 - 4778 - 98aa - 1aed98f7eed8
+//Обновить /help
+
+//Критерии оценки:
+//Пункты 1-7 - 8 баллов
+//Пункт 8 - 1 балл
+//Пункты 9-10 - 1 балл
+//Для зачёта домашнего задания достаточно 8 баллов.
---
+++ NailBot/UpdateHandler.cs
@@ -0,0 +1,130 @@
+﻿using Otus.ToDoList.ConsoleBot;
+using Otus.ToDoList.ConsoleBot.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NailBot
+{
+
+    internal class UpdateHandler : IUpdateHandler
+    {
+        enum Commands
+        {
+            Start = 1, Help, Info, Echo, Addtask, Showtasks, Removetask, Exit
+        }
+
+        static int echoNumber = (int)Commands.Echo;
+
+        //эхо
+        static bool availableEcho = false;
+
+        public bool answer = true;
+
+        public void HandleUpdateAsync(ITelegramBotClient botClient, Update update) {
+
+            Commands command;
+
+            string res = update.Message.Text;
+
+            string userName = "";
+
+            Chat chatId = update.Message.Chat;
+
+            try
+			{
+                //приведение к типу Enum
+                if (Enum.TryParse<Commands>(update.Message.Text, true, out var result))
+                    command = result;
+                else
+                    command = default;
+
+
+
+                switch (command)
+                {
+                    case Commands.Start:
+                        //Console.WriteLine("Введите ваше имя:");
+
+                        botClient.SendMessage(chatId, "Введите ваше имя:");
+
+
+                        userName = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(userName))
+                        {
+                            userName = char.ToUpper(userName[0]) + userName.Substring(1);
+                            Console.WriteLine($"Привет {userName}! Тебе стала доступна команда /echo");
+                            Console.WriteLine($"Чтобы использовать команду /echo, напиши \"/echo *свой текст*\"\n");
+                            availableEcho = true;
+                        }
+                        else
+                            Console.WriteLine("Привет, Незнакомец!");
+                        Commands.Start.CommandsRender(availableEcho, echoNumber);
+                        break;
+
+                    case Commands.Help:
+                        Init.ShowHelp(userName, availableEcho);
+                        break;
+
+                    case Commands.Info:
+                        Init.ShowInfo();
+                        break;
+
+                    case Commands.Echo:
+                        Console.WriteLine("ECHO");
+                        //Console.WriteLine(echoText);
+                        break;
+
+                    case Commands.Addtask:
+                        //вызов метода добавления задачи в List
+                        Init.AddTaskList(Init.tasksList, Init.maxTaskAmount, Init.maxTaskLenght);
+                        //вызов метода добавления задачи в Array
+                        //Init.AddTaskArray(ref Init.tasksArray, Init.maxTaskAmount, Init.maxTaskLenght);
+                        break;
+
+                    case Commands.Showtasks:
+                        //вызов метода рендера задач из List
+                        Init.ShowTasks(Init.tasksList);
+                        //вызов метода рендера задач из Array
+                        //Init.ShowTasks(ref Init.tasksArray);
+                        break;
+
+                    case Commands.Removetask:
+                        //вызов метода удаления задачи из List
+                        Init.RemoveTaskList(Init.tasksList);
+                        //вызов метода удаления задачи из Array
+                        //Init.RemoveTaskArray(ref Init.tasksArray);
+                        break;
+
+                    case Commands.Exit:
+                        Console.WriteLine("EXIT");
+                        answer = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка: введена некорректная команда. Пожалуйста, введите команду заново.\n");
+                        Commands.Start.CommandsRender(availableEcho, echoNumber);
+                        break;
+                }
+            }
+			catch (Exception ex)
+			{
+				throw new Exception("asdsadasd");
+			}
+
+        }
+
+
+    }
+}
+
+
+
+//Создать класс UpdateHandler, который реализует интерфейс IUpdateHandler,
+//и перенести в метод HandleUpdateAsync обработку всех команд. Вместо Console.WriteLine использовать SendMessage у ITelegramBotClient
+
+//Перенести try/catch в HandleUpdateAsync. В Main оставить catch(Exception)
+//Для вывода в коноль сообщений использовать метод ITelegramBotClient.SendMessage
