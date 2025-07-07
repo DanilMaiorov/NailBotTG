fa0ce3ce9def72a5711d42b17810df1325f10b13
Danil Uktus <damayorov93@gmail.com>
2025-07-07 22:06:21 +0500

    start hw_11

---
+++ NailBot/Core/Entities/ToDoList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NailBot.Core.Entities
+{
+    internal class ToDoList
+    {
+    }
+}
